4528cc5d11d37f1b314559955015e9fc852bea10
Ngadhenjim <nlubishtani@gmail.com>
2024-06-24 17:55:29 +0200

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+  <section className='hero' id='home'>
+    <div className='hero-banner'>
+      <h1> backroads app</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis vitae illum aperiam magni est voluptates iure enim reprehenderit? Aspernatur error iste quasi labore quidem necessitatibus quas earum consequatur ex quisquam.
+      </p>
+      <a href='#tours' className='btn hero-btn'>
+        explore tours
+      </a>
+    </div>
+  </section>
+  )
+}
+
+export default Hero
